543a319ac335a2c265c269a49980d57f122054bb
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-27 20:47:44 +0100

    Tested the server

---
+++ api/server.spec.js
@@ -0,0 +1,30 @@
+const request = require('supertest');
+const server = require('./server');
+
+describe('/ [GET]', () => {
+  it('returns a status 200', () =>
+    request(server)
+      .get('/')
+      .expect('Content-Type', /json/)
+      .expect(200));
+
+  it('returns a message', () =>
+    request(server)
+      .get('/')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then(res => {
+        expect(res.body.message).toEqual(
+          'The One Line a Day server is alive and kicking!',
+        );
+      }));
+
+  it('catches and handles bad request', () =>
+    request(server)
+      .get('/ver-inappropriate-url')
+      .expect('Content-Type', /json/)
+      .expect(404)
+      .then(res => {
+        expect(res.body.message).toEqual('Resource not found!');
+      }));
+});
