ef24ae3ad3e5ff670d5c80d005e8db7177318970
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-28 13:39:51 +0100

    The user table seeds

---
+++ database/seeds/01-users.js
@@ -0,0 +1,42 @@
+exports.seed = knex =>
+  knex('users')
+    .truncate()
+    .then(() =>
+      knex('users').insert([
+        {
+          firstname: 'John',
+          lastname: 'Doe',
+          username: 'jhdoe',
+          email: 'jh@john.com',
+          password: '12345',
+        },
+        {
+          firstname: 'Jane',
+          lastname: 'Doe',
+          username: 'jndoe',
+          email: 'jn@john.com',
+          password: '12345',
+        },
+        {
+          firstname: 'Will',
+          lastname: 'Smith',
+          username: 'freshPrince',
+          email: 'fresh@prince.com',
+          password: '12345',
+        },
+        {
+          firstname: 'Vin',
+          lastname: 'Diesel',
+          username: 'diesel',
+          email: 'vin@diesel.com',
+          password: '12345',
+        },
+        {
+          firstname: 'Van',
+          lastname: 'Damme',
+          username: 'van-damn',
+          email: 'van@damme.com',
+          password: '12345',
+        },
+      ]),
+    );
