cd8bf94661ee97bb51e041b7f4b068b4674b07b1
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-28 13:39:20 +0100

    The knex config

---
+++ knexfile.js
@@ -0,0 +1,21 @@
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+    },
+    useNullAsDefault: true,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
+  },
+};
