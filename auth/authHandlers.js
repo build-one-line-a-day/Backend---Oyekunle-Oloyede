69d358ce6eede716f5b067bf04a25e83a9e3bc2c
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-29 11:25:51 +0100

    Endpoint to log users in

--- auth/authHandlers.js
+++ auth/authHandlers.js
@@ -1,5 +1,6 @@
 const Model = require('./authModel');
 const bcryptHelper = require('../helpers/bcryptHelper');
+const { sign } = require('../helpers/jwtHelper');
 
 const getUsers = async (req, res) => {
   try {
@@ -67,8 +68,44 @@ const register = async (req, res) => {
   }
 };
 
+const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await Model.getByUsername(username);
+
+    if (!user)
+      return res.status(400).json({
+        status: 400,
+        message: 'Incorrect username',
+      });
+
+    const match = await bcryptHelper.compare(password, user.password);
+
+    if (!match)
+      return res.status(400).json({
+        status: 400,
+        message: 'Incorrect password.',
+      });
+
+    const token = await sign({ id: user.id });
+
+    res.status(200).json({
+      status: 200,
+      token,
+      message: 'Login successful.',
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: 500,
+      message: 'Cannot log user in.',
+    });
+  }
+};
+
 module.exports = {
   getUsers,
   getUserById,
   register,
+  login,
 };
--- auth/index.js
+++ auth/index.js
@@ -4,5 +4,6 @@ const handlers = require('./authHandlers');
 router.get('/users', handlers.getUsers);
 router.get('/users/:id', handlers.getUserById);
 router.post('/register', handlers.register);
+router.post('/login', handlers.login);
 
 module.exports = router;
