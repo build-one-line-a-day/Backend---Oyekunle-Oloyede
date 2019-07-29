51fd16fe7bfa474d8077cb76c4651e4b6e4c7f33
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-29 17:17:37 +0100

    Changed the name of the development config

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   development: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: process.env.DB_URL,
     useNullAsDefault: true,
     pool: {
       min: 2,
