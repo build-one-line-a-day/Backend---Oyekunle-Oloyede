2578ee143a9567d242d266d2446e37814969c16e
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-29 16:29:55 +0100

    Added a production config

--- knexfile.js
+++ knexfile.js
@@ -31,4 +31,19 @@ module.exports = {
       directory: './database/seeds',
     },
   },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    useNullAsDefault: true,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
+  },
 };
